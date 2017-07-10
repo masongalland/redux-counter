22cab93c220969d3c7f4d6019f9018e372176e5b
Mason Galland <masongalland@me.com>
2017-07-10 10:55:47 -0600

    step 1 done

--- src/ducks/counter.js
+++ src/ducks/counter.js
@@ -1,2 +1,10 @@
 // change this to true to see test results on the black diamond section.
 export const BLACK_DIAMOND = false;
+
+const initialState = {
+    currentValue: 0
+}
+
+export default function counter(state = initialState, action) {
+    return state;
+}
